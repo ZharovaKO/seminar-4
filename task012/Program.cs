a5921c2fe4d88a11a98a5c3d3eebc13b8fbb197d
ZharovaKO <xkseo05@yandex.ru>
2022-07-11 20:27:00 +0300

    task012

---
+++ task012/Program.cs
@@ -0,0 +1,14 @@
+﻿// Задача 27: Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.
+// 452 -> 11
+// 82 -> 10
+// 9012 -> 12
+Console.Write("Введите число: ");
+int number  = Convert.ToInt32(Console.ReadLine());
+int result = 0;
+while ( number > 0 )
+{
+    int modulo = number % 10;
+    number = number / 10;
+    result = result + modulo;
+}
+Console.WriteLine(result);
